b7d1da0c31c0703a12a42dc68911eedbc2d8e09d
kisung <abc1242@naver.com>
2022-06-25 18:41:23 +0900

    3주차6

--- 2주차/11866.요세푸스문제0.py
+++ 2주차/11866.요세푸스문제0.py
@@ -1,2 +1,32 @@
+from collections import deque
+
+
 N, K = map(int, input().split())
 
+arr = deque(i for i in range(1,N+1))
+res = []
+
+cnt = 0
+
+tmp = 0
+while len(arr) > 0:
+  cnt+=1
+  if cnt % K == 0:
+    tmp = arr.popleft()
+    res.append(tmp)
+  else:
+    tmp = arr.popleft()
+    arr.append(tmp)
+
+
+str1 ='<'
+for i in range(N):
+
+  str1 += str(res[i])
+
+  if i != N-1:
+    str1 += ', '
+
+str1 +='>'
+
+print(str1)
