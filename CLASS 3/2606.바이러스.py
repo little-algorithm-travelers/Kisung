3e42e3da23419e1b1c0f205de8b6d53c0a1238d5
kisung <abc1242@naver.com>
2022-07-20 10:08:42 +0900

    바이러스

---
+++ CLASS 3/2606.바이러스.py
@@ -0,0 +1,39 @@
+from queue import Queue
+
+
+N = int(input())
+
+M = int(input())
+
+arr = [[] for _ in range(N+1)]
+
+for _ in range(M):
+
+  a, b = map(int, input().split())
+
+  arr[a].append(b)
+  arr[b].append(a)
+
+print(arr)
+
+res = [0 for _ in range(N+1)]
+
+
+que = Queue()
+
+que.put(1)
+
+while not que.empty():
+
+  tmp = que.get()
+  if res[tmp] == 1:
+    continue
+
+  res[tmp] +=1
+
+  for i in arr[tmp]:
+
+    que.put(i)
+
+
+print(sum(res)-1)
