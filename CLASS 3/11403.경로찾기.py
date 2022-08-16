aab64c19291cc50db40dd8f65b3c86099d3a85d7
kisung <abc1242@naver.com>
2022-08-16 12:19:34 +0900

    경로찾기

--- CLASS 3/11403.경로찾기.py
+++ CLASS 3/11403.경로찾기.py
@@ -1,3 +1,10 @@
+
+
+from collections import deque
+
+
+
+
 N = int(input())
 
 arr = []
@@ -6,5 +13,33 @@ for _ in range(N):
 
   arr.append(list(map(int, input().split())))
 
-print(arr)
+res = [[ 0 for _ in range(N)] for _ in range(N)]
 
+
+
+def bfs(i):
+
+  Q = deque()
+
+
+  for j in range(N):
+
+    if arr[i][j] == 1:
+      Q.append([i,j])
+    
+  
+  while len(Q) > 0:
+
+    tmp1, tmp2 = Q.popleft()
+    res[i][tmp2] = 1
+
+    for k in range(N):
+
+      if arr[tmp2][k] == 1 and res[i][k] == 0:
+        Q.append([tmp2,k])
+
+for i in range(N):
+  bfs(i)
+
+for i in res:
+  print(*i)
