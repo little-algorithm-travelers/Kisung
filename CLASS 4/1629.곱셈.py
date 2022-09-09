af4fe13216386a4148a5a4bb7ebea692d9344169
kisung <abc1242@naver.com>
2022-09-09 18:00:38 +0900

    곱셈

---
+++ CLASS 4/1629.곱셈.py
@@ -0,0 +1,19 @@
+# a, b, c = map(int, input().split())
+
+# print(pow(a, b, c))
+
+
+import sys
+a,b,c = map(int,sys.stdin.readline().split())
+
+def multi (a,n):
+  if n == 1:
+      return a%c
+  else:
+      tmp = multi(a,n//2)
+      if n %2 ==0:
+          return (tmp * tmp) % c
+      else:
+          return (tmp  * tmp *a) %c
+          
+print(multi(a,b))
